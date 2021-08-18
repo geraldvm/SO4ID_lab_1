73c841727d07bdb6be9acfbd0ddd8a18774c0d49
Karina Martinez Guerrero <karinamguerrero@estudiantec.cr>
2021-08-18 14:22:14 -0600

    Cerficado ssl y redireccionamiento http

--- api/src/server.js
+++ api/src/server.js
@@ -3,10 +3,20 @@
 const express = require('express'); //Uses express module
 const morgan = require('morgan');
 const cors = require('cors');  //https://medium.com/zero-equals-false/using-cors-in-express-cac7e29b005b
-const app = express();
-const PORT = 1616; //Listen Port
 const swaggerJsDoc = require("swagger-jsdoc");
 const swaggerUi = require("swagger-ui-express");
+const https = require('https');
+const http = require('http')
+const fs = require('fs');
+
+const app = express();
+const PORT = 1616; //Listen Port
+const httpPort = 81;
+
+const options = {
+    key: fs.readFileSync(__dirname + '/localhost.decrypted.key', 'utf8'),
+    cert: fs.readFileSync(__dirname + '/localhost.crt', 'utf8')
+};
 
 const swaggerOptions = {
     swaggerDefinition: {
@@ -22,12 +32,13 @@ const swaggerOptions = {
             servers: ["http://localhost:1616"]
         }
     },
-    // ['.routes/*.js']
     apis: ['./src/routes/*.js']
 };
 
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+
 //Settings
 app.set('port', process.env.PORT || PORT);
 
@@ -47,11 +58,24 @@ app.use('/reservations', require('./routes/reservation'));
 //app.use('/api/parking', require('./routes/parking'));//poner por defecto la entrada /api/parking
 app.use(require('./routes/default'));
 
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(options, app);
 
+app.use((req, res, next) => {
+    if (req.protocol === 'http') {
+        res.redirect(301, `https://${req.headers.host}${req.url}`);
+    }
+    next();
+});
+
+httpServer.listen(httpPort, 'localhost');
+httpsServer.listen(PORT, 'localhost', () => {
+    console.log('server running at ' + PORT)
+})
 
 // starting the server
-app.listen(app.get('port'), () => {
+/*app.listen(app.get('port'), () => {
     console.log(`Server on port ${app.get('port')}`);
-});
+});*/
 
 
